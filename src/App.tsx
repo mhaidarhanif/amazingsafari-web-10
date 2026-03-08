e3a0d7a5efee0453a14eeef68a5fe23350f26b5d
M Haidar Hanif <git@mhaidarhanif.com>
2026-03-08 18:16:22 +0700

    feat: fetch products and display them

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import { ComponentExample } from "@/components/component-example";
+import { $api } from "./modules/common/api";
 
 export function App() {
-  return <ComponentExample />;
+  const {
+    data: products,
+    error,
+    isLoading,
+  } = $api.useQuery("get", "/products");
+  if (isLoading || !products) return <p>Loading products...</p>;
+  if (error) return <p>Load products error: ${error}</p>;
+
+  return (
+    <main>
+      <h1>Products</h1>
+
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              <h1>{product.name}</h1>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
 }
 
 export default App;
